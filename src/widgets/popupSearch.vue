63ec997a2484356f7832f237f92a09b75bdd3268
Taipa Xu <taipaxu@gmail.com>
2024-08-31 13:48:06 +0800

    automatically focus search input in popup page

--- src/widgets/popupSearch.vue
+++ src/widgets/popupSearch.vue
@@ -14,6 +14,7 @@
                 append-inner-icon="mdi-close"
                 single-line
                 hide-details
+                autofocus
                 @click:append-inner="$emits('exitSearchMode')"></v-text-field>
             </v-card-text>
         </v-card>
